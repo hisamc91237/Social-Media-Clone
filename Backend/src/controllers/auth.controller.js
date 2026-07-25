bd20d29d62789d89daf956ea472c37975e3c4b91
hisamc91237 <hisamc2000@gmail.com>
2026-07-25 23:53:10 +0530

    API Layer completed

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -95,7 +95,24 @@ const loginController = async (req, res) => {
   });
 };
 
+const getMeController = async (req, res) => {
+  const userId = req.user.id;
+
+  const user = await userModel.findById(userId);
+
+  res.status(200).json({
+    message: "user fetched successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profile_image: user.profile_image,
+    },
+  });
+};
+
 module.exports = {
   registerController,
   loginController,
+  getMeController,
 };
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
+const identifyUser = require("../middlewares/auth.middleware");
 
 const authRouter = express.Router();
 
@@ -12,5 +13,12 @@ authRouter.post("/register", authController.registerController);
 POST /api/auth/login
  */
 authRouter.post("/login", authController.loginController);
+
+/*
+ * @route : GET /api/auth/get-me
+ * @description : Get the login user data
+ * @access : private
+ */
+authRouter.get("/get-me", identifyUser, authController.getMeController);
 
 module.exports = authRouter;
