48c0626fa632ba5c348c2b18aad8a9ff7e0ed142
hisamc91237 <hisamc2000@gmail.com>
2026-07-26 18:41:47 +0530

    Register user completed

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import { Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [passeword, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const { loading, user, handleRegister } = useAuth();
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loaoding...</h1>
+      </main>
+    );
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    await handleRegister(username, email, passeword);
+    console.log("user registered");
+    navigate("/");
   };
 
   return (
@@ -11,9 +33,30 @@ const Register = () => {
       <div className="form-container">
         <h1>Register</h1>
         <form onSubmit={handleSubmit}>
-          <input type="text" name="username" placeholder="Enter username" />
-          <input type="text" name="email" placeholder="Enter email" />
-          <input type="password" name="password" placeholder="Enter passowrd" />
+          <input
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            placeholder="Enter username"
+          />
+          <input
+            onInput={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            name="email"
+            placeholder="Enter email"
+          />
+          <input
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            name="password"
+            placeholder="Enter passowrd"
+          />
           <button className="button primary-button">Register</button>
         </form>
         <p>
