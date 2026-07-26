dd379d76db212e607ceeb0ae38b30bc755b7d16f
hisamc91237 <hisamc2000@gmail.com>
2026-07-26 17:09:09 +0530

    State and Hook Layer

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../style/form.scss";
 import { Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import { useState } from "react";
 
 const Login = () => {
-  const handleSubmit = (e) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { user, loading, handleLogin } = useAuth();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    await handleLogin(username, password);
+    console.log("user loggedin");
   };
 
   return (
@@ -12,8 +22,22 @@ const Login = () => {
       <div className="form-container">
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-          <input type="text" name="username" placeholder="username" />
-          <input type="password" name="password" placeholder="password" />
+          <input
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            placeholder="username"
+          />
+          <input
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            name="password"
+            placeholder="password"
+          />
           <button className="button primary-button">Login</button>
         </form>
         <p>
